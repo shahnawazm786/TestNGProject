17ae0071a80c3c951abde384236e02d02d1ceca6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-16 15:39:32 +0530

    intial stage

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,14 +7,15 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
-	protected WebDriver driver;
+	protected static WebDriver driver;
+	private final String url="https://www.phptravels.net/";
 	@BeforeSuite
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com");
-		
+		//driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.get(url);		
 	}
 	@AfterSuite
 	public void teardown() {
--- src/test/java/tests/HomePageTestCase.java
+++ src/test/java/tests/HomePageTestCase.java
@@ -1,11 +1,14 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class HomePageTestCase extends BaseTest {
 
+	By anchorSignIn=By.linkText("Login");
 	@Test
-	public void loginTest() {
+	public void signInTest() {
+		driver.findElement(anchorSignIn).click();
 		
 	}
 }
