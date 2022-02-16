3e96167f4ea3cef7d85fff0c0e48b582c23a35ef
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-16 15:57:53 +0530

    TestNG

--- src/test/java/tests/HomePageTestCase.java
+++ src/test/java/tests/HomePageTestCase.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 public class HomePageTestCase extends BaseTest {
 
 	By anchorSignIn=By.linkText("Login");
+	By email=By.xpath("//input[@name='email']");
+	By pass=By.xpath("//input[@name='password']");
+	By btnLogin=By.xpath("//button[@type='submit']");
 	@Test
 	public void signInTest() {
 		driver.findElement(anchorSignIn).click();
