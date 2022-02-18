2c341e44c37fb351437aee33689a2e249f71af39
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-18 18:11:09 +0530

    instance

--- src/main/java/designpattern/SingletonClassExample.java
+++ src/main/java/designpattern/SingletonClassExample.java
@@ -2,7 +2,7 @@ package designpattern;
 
 public class SingletonClassExample {
 
-	public static volatile SingletonClassExample sce;
+	public static volatile SingletonClassExample sce=new SingletonClassExample();
 	private SingletonClassExample() {} // it is an example of singleton design pattern once you have created 
 	// private consturctor it is achieved
 	// 2nd object can be created by using reflection (loop hole)
