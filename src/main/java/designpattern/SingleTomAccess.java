abe9b3fbece25935b24c0148de9d3a503706678e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2022-02-18 18:09:08 +0530

    instance

--- src/main/java/designpattern/SingleTomAccess.java
+++ src/main/java/designpattern/SingleTomAccess.java
@@ -4,7 +4,10 @@ public class SingleTomAccess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SingletonClassExample sc=SingletonClassExample.getInstance();
+		System.out.println(sc);
+		SingletonClassExample sc1=SingletonClassExample.getInstance();
+		System.out.println(sc1);
 	}
 
 }
